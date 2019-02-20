f450ec92ed8fa5464a3ef8c5942286cbdb876e7a
DlSco <734808034@qq.com>
2019-02-20 14:11:23 +0800

    跨域处理修改

--- src/main/java/com/usedBooks/config/MyConfiguration.java
+++ src/main/java/com/usedBooks/config/MyConfiguration.java
@@ -1,5 +1,11 @@
 package com.usedBooks.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
 /**
  * 跨域处理
  */
@@ -8,7 +14,7 @@ public class MyConfiguration {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
+        return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**");
