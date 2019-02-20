f2c3d8c7da41c58157da0dcc4661a4d81e1ea726
DlSco <734808034@qq.com>
2019-02-20 14:09:46 +0800

    跨域处理，后期部署服务器采用nginx，现在先这样开发做着先

---
+++ src/main/java/com/usedBooks/config/MyConfiguration.java
@@ -0,0 +1,18 @@
+package com.usedBooks.config;
+
+/**
+ * 跨域处理
+ */
+@Configuration
+public class MyConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
