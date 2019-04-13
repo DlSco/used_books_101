b53560a893bf45bd52900f6a4b00929d43501243
DlSco <734808034@qq.com>
2019-04-13 20:15:12 +0800

    提交代码

--- src/main/java/com/usedBooks/Application.java
+++ src/main/java/com/usedBooks/Application.java
@@ -5,12 +5,18 @@ import tk.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @MapperScan("com.usedBooks.**.mapper")
 public class Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
+
+        TimeZone tz  =  TimeZone.getTimeZone( " Asia/Shanghai " );
+        TimeZone.setDefault(tz);
     }
 
 }
